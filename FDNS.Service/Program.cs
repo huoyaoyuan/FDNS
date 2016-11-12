4a77a7483aebfe2627bbee50e18551fdbece5dee
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-11-12 16:00:36 +0800

    创建项目。

---
+++ FDNS.Service/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceProcess;
+
+namespace FDNS.Service
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        private static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
